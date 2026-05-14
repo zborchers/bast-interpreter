4442d9768b9dfa5b972492431c2ad35d4a34a76b
zborchers <zborch@gmail.com>
2026-05-14 09:58:58 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BASTInterpreter from './BASTInterpreter'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BASTInterpreter />
+  </React.StrictMode>
+)
